b41546f8b347dfa450cda5d1fd2e696e172386bc
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2024-01-13 13:33:04 -0300

    .

--- frontend/src/app/components/login.tsx
+++ frontend/src/app/components/login.tsx
@@ -13,12 +13,15 @@ import { useUser } from "../hooks/useUser"
 import { useRouter } from "next/navigation"
 import { scpController } from "../controller/scpsController"
 import { useScp } from "../hooks/useScp"
+import { bankController } from "../controller/bankController"
+import { useBank } from "../hooks/useBank"
 
 
 export const Login = () => {
     const router = useRouter()
     const {setUser} = useUser()
     const {setScp} = useScp()
+    const {setBank} = useBank()
     
     const [name,setName] = useState<string>('')
     const [password,setPassword] = useState<string>('')
@@ -32,6 +35,7 @@ export const Login = () => {
             setUser(u)
             localStorage.setItem('id',u.id)
             scpController.getScps(u.id).then(setScp)
+            bankController.get(u.id).then(setBank)
             router.push('/library')
         })
         .catch(()=>alert('Senha ou nome inexistente'))
@@ -48,7 +52,7 @@ export const Login = () => {
     },[])
 
     return(
-        <div className="w-screen h-screen justify-center sm:w-auto sm:h-auto p-6 bg-gray-300 flex flex-col items-center rounded shadow-md gap-3">
+        <div className="w-screen h-screen justify-center sm:w-auto sm:h-auto sm:p-6 bg-gray-300 flex flex-col items-center rounded shadow-md gap-3">
             <IconScp />
             <Input onChange={e => setName(e.target.value)} Icon={User} placeholder="Nome"/>
             <Input onChange={e => setPassword(e.target.value)} Icon={Lock} placeholder="Senha"/>
