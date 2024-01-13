f90c8fd2e48cc97250cf8b145202ca605f193cdb
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2024-01-13 13:41:09 -0300

    Update login.tsx

--- frontend/src/app/components/login.tsx
+++ frontend/src/app/components/login.tsx
@@ -52,15 +52,18 @@ export const Login = () => {
     },[])
 
     return(
-        <div className="w-full h-full justify-center sm:w-auto sm:h-auto p-4 bg-gray-300 flex flex-col items-center rounded shadow-md gap-3">
-            <IconScp />
-            <Input onChange={e => setName(e.target.value)} Icon={User} placeholder="Nome"/>
-            <Input onChange={e => setPassword(e.target.value)} Icon={Lock} placeholder="Senha"/>
-            <div className="w-1/2">
-                <Button onClick={() => handleLogin()}>
-                    Entrar
-                </Button>
+        <div className="w-full h-full justify-center sm:w-auto sm:h-auto p-4 bg-gray-300 flex flex-col items-center rounded shadow-md">
+            <div className="max-w-[320px] flex flex-col items-center gap-3">
+                <IconScp />
+                <Input onChange={e => setName(e.target.value)} Icon={User} placeholder="Nome"/>
+                <Input onChange={e => setPassword(e.target.value)} Icon={Lock} placeholder="Senha"/>
+                <div className="w-1/2">
+                    <Button onClick={() => handleLogin()}>
+                        Entrar
+                    </Button>
+                </div>
             </div>
+
         </div>
     )
 }
